b1c867da93019b849546ff6bfcd74fc07abbbafe
savinovalex <savinov.ao@gmail.com>
2021-02-17 22:23:27 +0300

    load_config moved to utils/config_utils.py

--- run.py
+++ run.py
@@ -9,28 +9,7 @@ from configs.train_config import TrainConfig, BaseConfig
 import utils.timer as timer
 from utils.helpers import fix_seeds
 
-
-def load_config(config_path: str, rest_args: list) -> TrainConfig:
-    spec = importlib.util.spec_from_file_location(Path(config_path).stem, config_path)
-    config = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(config)
-
-    arg_tuples = []
-    while len(rest_args) > 0:
-        arg = rest_args.pop(0)
-        value = rest_args.pop(0)
-        if not arg.startswith('--'):
-            raise ValueError(f"Can't parse additional argument pair ({arg}, {value}")
-
-        arg_tuples.append((arg[2:], value))
-
-    cfg_args = dict(arg_tuples)
-    cfg = config.Config()
-    cfg.override_attrs(**cfg_args)
-    print('Config', cfg)
-
-    assert isinstance(cfg, BaseConfig)
-    return cfg
+from utils.config_utils import load_config
 
 
 if __name__ == '__main__':
---
+++ utils/config_utils.py
@@ -0,0 +1,26 @@
+import importlib.util
+from pathlib import Path
+from configs.train_config import TrainConfig, BaseConfig
+
+
+def load_config(config_path: str, rest_args: list) -> TrainConfig:
+    spec = importlib.util.spec_from_file_location(Path(config_path).stem, config_path)
+    config = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(config)
+
+    arg_tuples = []
+    while len(rest_args) > 0:
+        arg = rest_args.pop(0)
+        value = rest_args.pop(0)
+        if not arg.startswith('--'):
+            raise ValueError(f"Can't parse additional argument pair ({arg}, {value}")
+
+        arg_tuples.append((arg[2:], value))
+
+    cfg_args = dict(arg_tuples)
+    cfg = config.Config()
+    cfg.override_attrs(**cfg_args)
+    print('Config', cfg)
+
+    assert isinstance(cfg, BaseConfig)
+    return cfg
